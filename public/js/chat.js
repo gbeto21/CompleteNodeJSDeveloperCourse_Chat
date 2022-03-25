75316ef69a4ae42f38b7ded7a12d26edefc367fc
Alberto González <alberto.gonzalez@distillery.com>
2022-03-25 09:22:24 -0600

    Controls are disabled while the location is requested.

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,13 +1,24 @@
 const socket = io();
 
+// Elements
+const $messageForm = document.querySelector("#message-form");
+const $messageFormImput = document.querySelector("input");
+const $messageFormButton = document.querySelector("button");
+const $sendLocationButton = document.querySelector("#send-location");
+
 socket.on("message", (message) => {
   console.log(message);
 });
 
-document.querySelector("#message-form").addEventListener("submit", (e) => {
+$messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
+
+  $messageFormButton.setAttribute("disabled", "disabled");
   const message = e.target.elements.message.value;
   socket.emit("sendMessage", message, (error) => {
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormImput.value = "";
+    $messageFormImput.focus();
     if (error) {
       return console.error(error);
     }
@@ -15,10 +26,13 @@ document.querySelector("#message-form").addEventListener("submit", (e) => {
   });
 });
 
-document.querySelector("#send-location").addEventListener("click", () => {
+$sendLocationButton.addEventListener("click", () => {
   if (!navigator.geolocation) {
     return alert("Geolocation is nor supported by browser");
   }
+
+  $sendLocationButton.setAttribute("disabled", "disabled");
+
   navigator.geolocation.getCurrentPosition((position) => {
     socket.emit(
       "sendLocation",
@@ -27,6 +41,7 @@ document.querySelector("#send-location").addEventListener("click", () => {
         longitude: position.coords.longitude,
       },
       () => {
+        $sendLocationButton.removeAttribute("disabled");
         console.log("Location shared.");
       }
     );
